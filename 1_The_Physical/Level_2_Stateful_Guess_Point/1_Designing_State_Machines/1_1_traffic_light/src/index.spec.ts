b2d9571f92d28819faf96c50a5a8e54f2432ba22
Lilian Nguyen <24741666+lilian-n@users.noreply.github.com>
2023-05-27 14:22:56 -0600

    [Refactor]: create instance of traffic light for tests to share

--- 1_The_Physical/Level_2_Stateful_Guess_Point/1_Designing_State_Machines/1_1_traffic_light/src/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/1_Designing_State_Machines/1_1_traffic_light/src/index.spec.ts
@@ -1,31 +1,23 @@
 import { TrafficLight } from "./index";
 
 describe("traffic light", () => {
-  it("initially is off, with no colors shown", () => {
-    const trafficLight = TrafficLight.create();
+  const trafficLight = TrafficLight.create();
 
+  it("initially is off, with no colors shown", () => {
     expect(trafficLight.isSwitchedOn()).toBeFalsy();
     expect(trafficLight.getCurrentColor()).toBeNull();
   });
 
   it("can be turned on, with the color automatically green", () => {
-    // Arrange
-    const trafficLight = TrafficLight.create();
-
-    // Act
     trafficLight.switchOn();
 
-    // Assert
     expect(trafficLight.isSwitchedOn()).toBeTruthy();
     expect(trafficLight.getCurrentColor()).toBe("GREEN");
   });
 
   it("when on, changes color from green to yellow'", () => {
-    const trafficLight = TrafficLight.create();
-    trafficLight.switchOn();
-
     expect(trafficLight.getCurrentColor()).toBe("GREEN");
-    // Act
+
     trafficLight.changeColor();
 
     expect(trafficLight.getCurrentColor()).toBe("YELLOW");
