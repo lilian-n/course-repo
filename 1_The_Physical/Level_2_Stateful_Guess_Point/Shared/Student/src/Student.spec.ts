3880055520a60cca772cf0c4dbc48436da841478
Lilian Nguyen <24741666+lilian-n@users.noreply.github.com>
2023-05-28 13:15:02 -0600

    [New]: student isn't created when not given a last name

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/Student.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/Student.spec.ts
@@ -1,21 +1,29 @@
 import { Student } from "./Student";
 
 describe("Student", () => {
-  it("fails to be created when not given a first name", () => {
-    expect(() => Student.create(null, "Boop")).toThrowError();
+  describe("when defining a first name", () => {
+    it("fails to be created when not given a first name", () => {
+      expect(() => Student.create(null, "Boop")).toThrowError();
+    });
+
+    it("fails to be created when the first name is only 1 character", () => {
+      expect(() => Student.create("B", "Boop")).toThrowError();
+    });
+
+    it("fails to be created when the first name is over 10 characters", () => {
+      expect(() => Student.create("Bettyyyyyyy", "Boop")).toThrowError();
+    });
+
+    it("succeeds in being created when the first name is 5 characters, between 2 and 10 characters", () => {
+      const student = Student.create("Betty", "Boop");
+
+      expect(student.getFirstName()).toBe("Betty");
+    });
   });
 
-  it("fails to be created when the first name is only 1 character", () => {
-    expect(() => Student.create("B", "Boop")).toThrowError();
-  });
-
-  it("fails to be created when the first name is over 10 characters", () => {
-    expect(() => Student.create("Bettyyyyyyy", "Boop")).toThrowError();
-  });
-
-  it("succeeds in being created when the first name is 5 characters, between 2 and 10 characters", () => {
-    const student = Student.create("Betty", "Boop");
-
-    expect(student.getFirstName()).toBe("Betty");
+  describe("when defining a last name", () => {
+    it("fails to be created when not given a last name", () => {
+      expect(() => Student.create("Betty", null)).toThrowError();
+    });
   });
 });
