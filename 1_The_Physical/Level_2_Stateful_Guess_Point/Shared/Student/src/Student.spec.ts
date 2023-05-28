1a3b187b9b848e82337408ec82007e89fe2b6bff
Lilian Nguyen <24741666+lilian-n@users.noreply.github.com>
2023-05-28 13:12:13 -0600

    [Refactor]: unify language from made to created

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/Student.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/Student.spec.ts
@@ -1,15 +1,15 @@
 import { Student } from "./Student";
 
 describe("Student", () => {
-  it("fails to be made when not given a first name", () => {
+  it("fails to be created when not given a first name", () => {
     expect(() => Student.create(null, "Boop")).toThrowError();
   });
 
-  it("fails to be made when the first name is only 1 character", () => {
+  it("fails to be created when the first name is only 1 character", () => {
     expect(() => Student.create("B", "Boop")).toThrowError();
   });
 
-  it("fails to be made when the first name is over 10 characters", () => {
+  it("fails to be created when the first name is over 10 characters", () => {
     expect(() => Student.create("Bettyyyyyyy", "Boop")).toThrowError();
   });
 });
