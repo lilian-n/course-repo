18cf4df96b160ec584caacddbceaf1e8946e21f8
Lilian Nguyen <24741666+lilian-n@users.noreply.github.com>
2023-05-28 19:47:56 -0600

    [Refactor]: tests to be more specific

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/Student.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/Student.spec.ts
@@ -41,22 +41,23 @@ describe("Student", () => {
     });
   });
 
-  describe("when automatically generating a student email", () => {
+  describe("when automatically generating a student email, should define in the following format: up to 5 characters of their last name + 2 characters of their first name + @essentialist.dev", () => {
     it("should have a student email defined", () => {
       const student = Student.create("Betty", "Boop");
-
       expect(student.getEmail()).toBeDefined();
     });
 
-    it("should define their email as all lowercase characters", () => {
+    it("given the name 'Betty Boop', should create 'boopbe@essentialist.dev' ", () => {
       const student = Student.create("Betty", "Boop");
       expect(student.getEmail()).toBe("boopbe@essentialist.dev");
     });
 
-    it("should define their email with up to the first 5 characters of their last name", () => {
+    it("given the name 'Khalil Stemmler', should create 'stemmkh@essentialist.dev' ", () => {
       const student = Student.create("Khalil", "Stemmler");
       expect(student.getEmail()).toBe("stemmkh@essentialist.dev");
+    });
 
+    it("given the name 'Maxwell Po', should create 'poma@essentialist.dev'", () => {
       const anotherStudent = Student.create("Maxwell", "Po");
       expect(anotherStudent.getEmail()).toBe("poma@essentialist.dev");
     });
